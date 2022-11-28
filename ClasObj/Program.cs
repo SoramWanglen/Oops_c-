5612a1832fa693c78ef8e5e34506c5327cb243cd
SoramWanglen <soramwanglen.net@gmail.com>
2022-11-28 14:36:58 +0530

    oops concepts

---
+++ ClasObj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restaurant
+{
+    class ResClaObj
+    {
+        string name = "abc";
+        static void Main(string[] args)
+        {
+            ResClaObj rest = new ResClaObj();
+            rest.name = "pecos";
+            Console.WriteLine(rest.name);
+        }
+    }
+}
---
+++ ClasObj/ResInheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+    {
+    class Pecos
+    {
+        public string star = "**";
+
+        public void chef()
+        {
+            Console.WriteLine("all the chefs are promising!");
+        }
+    }
+    class Barbeque : Pecos
+    {
+        public string address = "Koramangala";
+    }
+    class ResInheritance
+    {
+        static void main(string[] args)
+        {
+            Barbeque berbeque = new Barbeque();
+            berbeque.chef();
+            Console.WriteLine("pecos is " + berbeque.star + " is located at" + berbeque.address);
+
+        }
+    }
+}
